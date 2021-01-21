21a9fcc1dc3c9be84442ffe116902cdca2a42581
tryhardsnipehrd <57113411+tryhardsnipehrd@users.noreply.github.com>
2021-01-21 13:20:42 -0700

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,3 @@
+main.c is the source
+a.exe is the output of "gcc main.c"
+debug.exe is the output of "gcc main.c -d -o debug.exe"
---
+++ password-hasher/main.c
@@ -0,0 +1,56 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+
+static char input[100];
+
+static int getInput() {
+	return fgets(input, sizeof(input), stdin) != NULL;
+}
+
+static int parseAndExecute() {
+	char *password = strtok(input, " \n");
+	if (password != NULL) {
+		if (strcmp(password, "quit") == 0) {
+			return 0;
+		}
+		if (strcmp(password, "read") == 0) {
+			openFile();
+		}
+		else {
+			printf("You entered: '%s'\n", password);
+		}
+	}
+	if (password == NULL) {
+		printf("You entered nothing...\n");
+	}
+	return 1;
+}
+
+int openFile() {
+	char header[8];
+	int bufferSize = strlen(header);
+	char headerContents[8];
+    FILE *fp; // declaration of file pointer
+    char con[1000]; // variable to read the content
+    fp = fopen("file.tin","r");// opening of file
+    if (!fp) {
+		return 1;
+	};
+	printf(sizeof(fread(header, bufferSize, 1, fp)));
+	printf("\n");
+    while (fgets(con,1000, fp)!=NULL)// reading file content
+    printf("%s",con);
+	printf("\n");
+    fclose(fp); // closing file
+    return 1;
+}
+
+int main() {
+	printf("welcome!\n");
+	while (getInput() && parseAndExecute());
+	return 0;
+}
+
+	
